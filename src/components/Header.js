01a26450a8b4e01d6a197ad54d9fd3bf62ef9a66
Freddy Ramírez <furede87@gmail.com>
2020-04-03 17:30:53 -0600

    Update Header to be a Stateless Component
    - Use of props to render data

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,25 @@
 import React from 'react';
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header className='top'>
-        <h1>
-          Catch
-          <span className='ofThe'>
-            <span className='of'>Of</span>
-            <span className='the'>The</span>
-          </span>
-          Day
-        </h1>
-        <h3 className='tagline'>
-          <span>Fresh Sea Food Market</span>
-        </h3>
-      </header>
-    )
-  }
-}
+// if we are just passing data via props and rendering it here, we
+// migth as well use a <<stateless component>> instead of extending
+// the whole <<React.Component>> library which would be an overkill
+
+const Header = (props) => (
+  <header className='top'>
+    <h1>
+      Catch
+      <span className='ofThe'>
+        <span className='of'>Of</span>
+        <span className='the'>The</span>
+      </span>
+      Day
+    </h1>
+    <h3 className='tagline'>
+      <span>
+        {props.tagline}
+      </span>
+    </h3>
+  </header>
+);
 
 export default Header;
