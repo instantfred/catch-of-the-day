2f5931927742fcb3b0a264923491bc388dd38232
Freddy Ramírez <furede87@gmail.com>
2020-04-05 21:16:46 -0600

    Add objects to the State via form
    We need to declare the State at the lowest level so that every component has access to it. Then we can pass down the events/functions as props to the their children

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,23 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 
-
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+  addFish = (fish) => {
+    // To update the state we need to do the following:
+    // 1. Take a copy of the existing state. We never want to update our state directly
+    const fishes = { ...this.state.fishes };
+    // 2. Add any new element to our copy variable
+    fishes[`fish${Date.now()}`] = fish;
+    // 3. Set the new fishes object to state
+    this.setState({
+      fishes: fishes
+    });
+  };
+
   render(){
     return (
       <div className='catch-of-the-day'>
@@ -12,7 +27,7 @@ class App extends React.Component {
           <Header tagline='Fresh Seafood Market' />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     )
   }
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import AddFishForm from './AddFishForm'
 
 class Inventory extends React.Component {
   render () {
     return (
       <div className='inventory'>
-        Inventory!!!
+        <h2>Inventory</h2>
+        {/* we pass the function using props, so thats why we need to invoke them here */}
+        <AddFishForm addFish={this.props.addFish} />
       </div>
     )
   }
