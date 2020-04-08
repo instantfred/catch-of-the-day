1135ffc8878ab880a682815e263bc7aec37a2c27
Freddy Ramírez <furede87@gmail.com>
2020-04-07 18:30:18 -0600

    Load sample fishes into state via onClick event

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
+
+  // Any function the updates the state needs to live where the state is defined
   state = {
     fishes: {},
     order: {}
@@ -20,6 +23,11 @@ class App extends React.Component {
     });
   };
 
+  loadSampleFishes = () => {
+    // this is the json data we imported, so we are just adding it to the state
+    this.setState({ fishes: sampleFishes });
+  };
+
   render(){
     return (
       <div className='catch-of-the-day'>
@@ -27,7 +35,10 @@ class App extends React.Component {
           <Header tagline='Fresh Seafood Market' />
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     )
   }
